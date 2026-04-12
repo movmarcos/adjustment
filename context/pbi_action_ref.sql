1c20d3555678c3e10452dc69dff63ba7711222f8
Marcos Magri <marcos.magri@mufgsecurities.com>
2026-04-12 21:50:46 +0100

    pbi action ref report

---
+++ context/pbi_action_ref.sql
@@ -0,0 +1,39 @@
+create or replace view DVLP_RAPTOR_NEWADJ.METADATA.VW_POWERBI_PROCESSING(
+	COBID,
+	STATUS,
+	WORKSPACE_NAME,
+	PROCESS_TYPE,
+	OBJECT_NAME,
+	OBJECT_TYPE,
+	INSERT_SOURCE,
+	REQUEST_TIME,
+	START_TIME,
+	COMPLETE_TIME
+) as
+SELECT 
+	COBID,
+	CASE 
+		WHEN START_TIME IS NULL THEN 'Queued'			
+		WHEN COMPLETE_TIME IS NULL THEN 'Running'
+		ELSE 'Completed'
+	END AS STATUS,
+ 	WORKSPACE_NAME , 
+ 	CASE 
+ 		WHEN OBJECT_NAME = 'RaptorReporting' AND INSERT_SOURCE LIKE 'VAR%' THEN 'VaR'
+ 		WHEN OBJECT_NAME = 'RaptorReporting' AND INSERT_SOURCE LIKE 'Stress%' THEN 'Stress'
+ 		WHEN OBJECT_NAME = 'RaptorReporting' AND INSERT_SOURCE LIKE 'SENSITIVITY%' THEN 'Sensitivity'
+ 		WHEN OBJECT_NAME = 'VaR Adjustment Summary Import' THEN 'VaR Adjustment'
+  		WHEN OBJECT_NAME = 'Stress Measures Adjustment Import' THEN 'Stress Adjustment'
+  		WHEN OBJECT_NAME = 'Sensitivity Summary Adjustment Import' THEN 'Sensitivity Adjustment'
+  		WHEN OBJECT_NAME IN ('Stress Measures Import', 'Stress Measures Import Cyclic') THEN 'Stress Adhoc'
+  		WHEN OBJECT_NAME IN ('VaR Book Import', 'VAR_SUMMARY_IMPORT_CYCLIC') THEN 'VaR Adhoc'
+  		WHEN OBJECT_NAME = 'Sensitivity Summary Import' THEN 'Sensitivity Adhoc'  		
+  		WHEN OBJECT_NAME = 'VAR_SUMMARY_IMPORT_MAX' THEN 'VaR 10Day/Maximiser'
+  	END AS PROCESS_TYPE,
+ 	OBJECT_NAME, 
+ 	OBJECT_TYPE,
+ 	INSERT_SOURCE, 
+ 	convert_timezone( 'UTC','Europe/London', REQUEST_TIME::timestamp_ntz) REQUEST_TIME, 
+    convert_timezone( 'UTC','Europe/London', START_TIME::timestamp_ntz) START_TIME, 
+    convert_timezone( 'UTC','Europe/London', COMPLETE_TIME::timestamp_ntz) COMPLETE_TIME
+FROM METADATA.POWERBI_ACTION;
